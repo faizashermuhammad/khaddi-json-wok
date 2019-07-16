43b36976d3f2a6dd4667606587dc13fd10bd20f4
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-07-16 00:47:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,156 @@
+let khaadi = {
+    about: {
+        outletName: "khaadi",
+        city: "karachi",
+        conactNumber: "03456787",
+    },
+
+    mewInWoman: {
+        unstiched: "",
+        pret: "",
+        khaas: "",
+        bottum: "",
+    },
+    newInkids: {
+        boy: "",
+        boy: "",
+    },
+    newINaccessories: {
+        woman: "",
+        man: "",
+        others: "",
+    },
+    newInhome: {
+        badroom: "",
+        bathroom: "",
+        dinningRoom: "",
+        cushionCover: "",
+        kidsCollection: "",
+    },
+    womanUnstiched: {
+        summmerFestivalCollection: "",
+        eidLuxuyCollection: "",
+        summer2019: "",
+        spring2019: "",
+    },
+    pret: {
+        basicsKurta: "",
+        printedKurta: "",
+        kurtaWithDupatta: "",
+        embroidedKurta: "",
+        fullSuit: "",
+        kurtaWihPants: "",
+
+    },
+    khaas: {
+        semiFormal: "",
+        casual: "",
+    },
+    bottom: {
+        formalPants: "",
+        pants: "",
+        shalwar: "",
+        tights: "",
+        tenim: "",
+    },
+
+    kidsGirlsEastern: {
+        kurta: "",
+        pants: "",
+        formalSuits: "",
+        trousers: "",
+    },
+    girlsWesterns: {
+        pants: "",
+        shalwar: "",
+        tights: "",
+        tenim: "",
+        Tsharts: "",
+        dressess: "",
+    },
+    boyEastern: {
+        kurta: "",
+        waistCoat: "",
+    },
+
+    boyWestern: {
+        denim: "",
+        tshart: "",
+        sharts: "",
+        shorts: "",
+
+    },
+    easternAccessories: {
+        boxcer: "",
+        hairAccessories: "",
+        socks: "",
+    },
+    accessoriesWoman: {
+        handBags: "",
+        clutches: "",
+        wallet: "",
+        scarves: "",
+        stoles: "",
+        jewellary: "",
+        footwear: "",
+        duptta: "",
+        hairAccessories: "",
+        sunglass: "",
+        tankTops: "",
+    },
+    man: {
+        perfumes: "",
+    },
+    girls: {
+        bags: "",
+        tops: "",
+        hairAccessories: "",
+        stoles: "",
+        jewellery: "",
+    },
+    homeBadroomcollecion: {
+        badCovers: "",
+        duvetCovers: "",
+        floorRunner: "",
+        pillowCovers: "",
+        fittedSheets: "",
+    },
+    bathroom: {
+        showerCurtain: "",
+        towels: "",
+    },
+    dinningRoom: {
+        tablesCover: "",
+        tableRunner: "",
+        handwoven: "",
+        placemats: "",
+        hotPlates: "",
+        costers: "",
+        rotiCloths: "",
+        embroided: "",
+    },
+    cushionCover: {
+        handWoven: "",
+        embroidred: "",
+        digitalPrinter: "",
+    },
+    kidsCollection: {
+        badsheet: "",
+        cushionCover: "",
+        duvetCovers: "",
+        towels: "",
+        cushionCover: "",
+        fittedSheets: "",
+    },
+
+    beautyByBrand: {
+            fragrences: "",
+            soakingSensation: "",
+            perfumes: "",
+            
+        },
+
+    }
+
+
+
