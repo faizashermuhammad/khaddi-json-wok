a050b2465ebca8b09eada9dc2abb669de314418b
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-07-16 05:48:57 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,155 +2,255 @@ let khaadi = {
     about: {
         outletName: "khaadi",
         city: "karachi",
-        conactNumber: "03456787",
+        conactNumber: "03456787"
     },
 
-    mewInWoman: {
-        unstiched: "",
-        pret: "",
-        khaas: "",
-        bottum: "",
+    mewIn: {
+        Woman: {
+            unstiched: "",
+            pret: "",
+            khaas: "",
+            bottum: ""
+        },
+        kids: {
+            boy: "",
+            girl: ""
+        },
+        accessories: {
+            woman: "",
+            man: "",
+            others: ""
+        },
+        home: {
+            badroom: "",
+            bathroom: "",
+            dinningRoom: "",
+            cushionCover: "",
+            kidsCollection: ""
+        },
     },
-    newInkids: {
-        boy: "",
-        boy: "",
-    },
-    newINaccessories: {
-        woman: "",
-        man: "",
-        others: "",
-    },
-    newInhome: {
-        badroom: "",
-        bathroom: "",
-        dinningRoom: "",
-        cushionCover: "",
-        kidsCollection: "",
-    },
-    womanUnstiched: {
-        summmerFestivalCollection: "",
-        eidLuxuyCollection: "",
-        summer2019: "",
-        spring2019: "",
-    },
-    pret: {
-        basicsKurta: "",
-        printedKurta: "",
-        kurtaWithDupatta: "",
-        embroidedKurta: "",
-        fullSuit: "",
-        kurtaWihPants: "",
+    woman: {
+        Unstiched: {
+            summmerFestivalCollection: "",
+            eidLuxuyCollection: "",
+            summer2019: "",
+            spring2019: ""
+        },
+        pret: {
+            basicsKurta: "",
+            printedKurta: "",
+            kurtaWithDupatta: "",
+            embroidedKurta: "",
+            fullSuit: "",
+            kurtaWihPants: ""
 
-    },
-    khaas: {
-        semiFormal: "",
-        casual: "",
-    },
-    bottom: {
-        formalPants: "",
-        pants: "",
-        shalwar: "",
-        tights: "",
-        tenim: "",
-    },
-
-    kidsGirlsEastern: {
-        kurta: "",
-        pants: "",
-        formalSuits: "",
-        trousers: "",
-    },
-    girlsWesterns: {
-        pants: "",
-        shalwar: "",
-        tights: "",
-        tenim: "",
-        Tsharts: "",
-        dressess: "",
-    },
-    boyEastern: {
-        kurta: "",
-        waistCoat: "",
+        },
+        khaas: {
+            semiFormal: "",
+            casual: ""
+        },
+        bottom: {
+            formalPants: "",
+            pants: "",
+            shalwar: "",
+            tights: "",
+            tenim: ""
+        },
+        Category: {
+            price: "",
+            color: "",
+            material: ""
+        },
     },
 
-    boyWestern: {
-        denim: "",
-        tshart: "",
-        sharts: "",
-        shorts: "",
-
-    },
-    easternAccessories: {
-        boxcer: "",
-        hairAccessories: "",
-        socks: "",
-    },
-    accessoriesWoman: {
-        handBags: "",
-        clutches: "",
-        wallet: "",
-        scarves: "",
-        stoles: "",
-        jewellary: "",
-        footwear: "",
-        duptta: "",
-        hairAccessories: "",
-        sunglass: "",
-        tankTops: "",
-    },
-    man: {
-        perfumes: "",
-    },
-    girls: {
-        bags: "",
-        tops: "",
-        hairAccessories: "",
-        stoles: "",
-        jewellery: "",
-    },
-    homeBadroomcollecion: {
-        badCovers: "",
-        duvetCovers: "",
-        floorRunner: "",
-        pillowCovers: "",
-        fittedSheets: "",
-    },
-    bathroom: {
-        showerCurtain: "",
-        towels: "",
-    },
-    dinningRoom: {
-        tablesCover: "",
-        tableRunner: "",
-        handwoven: "",
-        placemats: "",
-        hotPlates: "",
-        costers: "",
-        rotiCloths: "",
-        embroided: "",
-    },
-    cushionCover: {
-        handWoven: "",
-        embroidred: "",
-        digitalPrinter: "",
-    },
-    kidsCollection: {
-        badsheet: "",
-        cushionCover: "",
-        duvetCovers: "",
-        towels: "",
-        cushionCover: "",
-        fittedSheets: "",
+    newCollection2019:{
+        eidLuxuyCollection:{
+            formalKurta: "",
+            casualKurta: "",
+            kurtaWithDupatta: "",
+            embroidedKurta: "",
+            fullSuit: "",
+            kurtaWihPants: ""
+        },
+        partWear:{
+            
+        }
     },
 
-    beautyByBrand: {
+    kids: {
+        GirlsEastern: {
+            kurta: "",
+            pants: "",
+            formalSuits: "",
+            trousers: "",
+        },
+        girlsWesterns: {
+            pants: "",
+            shalwar: "",
+            tights: "",
+            tenim: "",
+            Tsharts: "",
+            dressess: "",
+        },
+        boyEastern: {
+            kurta: "",
+            waistCoat: "",
+        },
+
+        boyWestern: {
+            denim: "",
+            tshart: "",
+            sharts: "",
+            shorts: "",
+
+        },
+        easternAccessories: {
+            boxcer: "",
+            hairAccessories: "",
+            socks: "",
+        },
+        Category: {
+            price: "",
+            color: "",
+            material: ""
+        },
+    },
+    accessories: {
+        Woman: {
+            handBags: "",
+            clutches: "",
+            wallet: "",
+            scarves: "",
+            stoles: "",
+            jewellary: "",
+            footwear: "",
+            duptta: "",
+            hairAccessories: "",
+            sunglass: "",
+            tankTops: "",
+        },
+        man: {
+            perfumes: "",
+        },
+        girls: {
+            bags: "",
+            tops: "",
+            hairAccessories: "",
+            stoles: "",
+            jewellery: "",
+        },
+    },
+    home: {
+        Badroomcollecion: {
+            badCovers: "",
+            duvetCovers: "",
+            floorRunner: "",
+            pillowCovers: "",
+            fittedSheets: "",
+        },
+        bathroom: {
+            showerCurtain: "",
+            towels: "",
+        },
+        dinningRoom: {
+            tablesCover: "",
+            tableRunner: "",
+            handwoven: "",
+            placemats: "",
+            hotPlates: "",
+            costers: "",
+            rotiCloths: "",
+            embroided: "",
+        },
+        cushionCover: {
+            handWoven: "",
+            embroidred: "",
+            digitalPrinter: "",
+        },
+        kidsCollection: {
+            badsheet: "",
+            cushionCover: "",
+            duvetCovers: "",
+            towels: "",
+            cushionCover: "",
+            fittedSheets: "",
+        },
+    },
+
+    beauty: {
+        ByBrand: {
             fragrences: "",
             soakingSensation: "",
             perfumes: "",
-            
-        },
+            pureIntention: "",
+            tressedUp: ""
 
-    }
+        },
+        ByCategory: {
+            mosturisures: "",
+            cleaners: "",
+            exsolaitors: "",
+            bodyOrHandCare: "",
+            lipsCare: "",
+            masks: "",
+            hairCare: ""
+
+        },
+        byconcernse: {
+            dryness: "",
+            acne: "",
+            oilControl: "",
+            ageProvention: "",
+            darkSpots: "",
+            unevenSkinTone: "",
+            pores: "",
+            lackorRadience: "",
+            linseWrinkles: ""
+        },
+    },
+    gift: {
+        giftCards: "",
+        giftbox: ""
+    },
+    sale: {
+        woman: {
+            sale: "50%off",
+        },
+        kidsCollection: {
+            sale: "70%off",
+        },
+        home: {
+            sale: "80%off",
+        },
+        acccessories: {
+            sale: "70%off",
+        },
+    },
+    company: {
+        aboutUs: "",
+        careersatKhaadi: "",
+        contactUs: ""
+
+    },
+
+    customerSupport: {
+        customerServices: "",
+        shipping: "",
+        handling: "",
+        storeLucater: "",
+        return: "",
+        exchanges: "",
+        disclaimer: ""
+    },
+
+    help: {
+        trackyourOrder: "",
+        termofConditions: "",
+        privacyPolicy: "",
+        giftCardFqas: "",
+    },
+    
+}
 
 
 
